08f0be13a4d04ad27ca505645fde580e0cadaea4
jupo13 <ghghtygh@naver.com>
2025-07-11 14:09:58 +0900

    first commit

---
+++ coupon-core/src/test/java/com/gpg/couponcore/CouponCoreConfigurationTests.java
@@ -0,0 +1,13 @@
+package com.gpg.couponcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponCoreConfigurationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
